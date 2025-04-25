cfbe4903379b23ab0c376ace71affce6a70e297c
jacoblewis046 <jacoblewis046@gmail.com>
2025-04-25 14:00:27 +0100

    added infinities to initial max and min, made maxmin a function aswell.

--- maxmin.py
+++ maxmin.py
@@ -3,18 +3,21 @@ def str_to_list(str):
     str_list = str[1:-1].split(",")
     return [int(x) for x in str_list]
 
+def maxmin(ints_list):
+    min = float("inf")
+    max = float("-inf")
+
+    for num in ints_list:
+        if num > max:
+            max = num
+        if num < min:
+            min = num
+
+    return [min,max]
+
 
 ints = input("Enter your ints: ")
 ints_list = str_to_list(ints)
-# print(ints_list)
+print(maxmin(ints_list))
 
-min = 10000000000000
-max = 0
 
-for num in ints_list:
-    if num > max:
-        max = num
-    if num < min:
-        min = num
-
-print([min,max])
